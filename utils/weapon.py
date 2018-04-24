770ceef355a920af149d80d191d26ffea7a85280
SpearVFX <alexander.kostov37@gmail.com>
2018-04-24 12:32:12 +0300

    Adding first weapon file

---
+++ utils/weapon.py
@@ -0,0 +1,12 @@
+
+"""
+In order for our hero to have proper damage,
+he must be equiped with either a weapon or a spell.
+One hero can carry at max 1 weapon and 1 spell.
+"""
+
+
+class Weapon:
+    def __init__(self, *, name="", damage=0)
+        self.name = name
+        self.damage = damage
