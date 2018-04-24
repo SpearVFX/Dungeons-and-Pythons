e1ecfcee810aaa781d1b10cb35911df62f76d3f5
Shady Dealer <shady>
2018-04-24 23:31:01 +0300

    Added a missed : symbol.

--- utils/spell.py
+++ utils/spell.py
@@ -1,10 +1,10 @@
 class Spell:
-    def __init__(self, *, name="",damage=0, mana_cost=0, cast_range=0):
+    def __init__(self, *, name="",damage=0, manaCost=0, castRange=0):
         self.name = name
         self.damage = damage
-        self.mana_cost = mana_cost # the spell needs at least that much amount of mana in order to be cast.
+        self.manaCost = manaCost # the spell needs at least that much amount of mana in order to be cast.
                                    # Raise an error if you cannot cast that spell.
-        self.cast_range = cast_range # if cast range is 1 you can attack enemies next you.
+        self.castRange = castRange # if cast range is 1 you can attack enemies next you.
 
     def __str__(self):
         return self.name
