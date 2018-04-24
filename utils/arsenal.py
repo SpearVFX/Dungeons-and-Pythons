5b23c1ab4ca6e8fc8bf4cca2a432d847d95652cb
Shady Dealer <shady>
2018-04-24 23:41:32 +0300

    Added getter methods for the weapon and spell attributes.

--- utils/arsenal.py
+++ utils/arsenal.py
@@ -6,10 +6,17 @@ class Arsenal:
     def __init__(self, weapon = None, spell = None):
         self.__weapon = weapon
         self.__spell = spell
-
+    
+    ''' Switches the arsenals current weapon with a new one. '''
     def equip_weapon(self, weapon = None):
         self.__weapon = weapon
-    
+    ''' Switches the aresanls current spell with a new one. '''
     def learn_spell(self, spell= None):
         self.__spell = spell
 
+    ''' Getter methods '''
+    def get_weapon(self):
+        return str(self.__weapon)
+    
+    def get_spell(self):
+        return str(self.__spell)
