de45f8cc9b7fd39bfa93ad4b8b419ac652de333e
Shady Dealer <dimiturlukanov@gmail.com>
2018-04-26 18:39:46 +0300

    Added a test module for the enemy module.

---
+++ utils/enemy_tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+from enemy import Enemy
+
+
+class EnemyTests(unittest.TestCase):
+    def setUp(self):
+        self.dummy = Enemy(health=100, mana=100, damage=20)
+    
+    def test_get_base_damage_method(self):
+        self.assertEqual(self.dummy.get_base_damage(), 20)
+
+
+if __name__ == '__main__':
+    unittest.main()
