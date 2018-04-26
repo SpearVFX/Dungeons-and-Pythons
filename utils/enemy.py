938557ede02a71cc0bf468529a1a81a5128ebb80
Shady Dealer <dimiturlukanov@gmail.com>
2018-04-26 18:35:11 +0300

    Added and finished the enemy module.

---
+++ utils/enemy.py
@@ -0,0 +1,9 @@
+from entity import Entity
+
+class Enemy(Entity):
+    def __init__(self,*,health=100, mana=100, damage=20):
+        super().__init__(health=health, mana=mana)
+        self.__baseDamage = damage
+
+    def get_base_damage(self):
+        return self.__baseDamage
