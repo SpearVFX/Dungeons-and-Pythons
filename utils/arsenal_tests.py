0f4247fbd2bc7e89c953bfab7ee6665f299f91ff
Shady Dealer <dimiturlukanov@gmail.com>
2018-04-26 17:40:57 +0300

    Added tests for the equip_weapon method, the learn_spell method and for the getters.

--- utils/arsenal_tests.py
+++ utils/arsenal_tests.py
@@ -7,12 +7,28 @@ from spell import Spell
 
 class ArsenalUnitTests(unittest.TestCase):
     
-    def test_Arsenal_object_creation(self):
+    def setUp(self):
+        self.dummy = Arsenal()
+        self.weapon = Weapon(name = "Gorehowl", damage = 20)
+        self.spell = Spell(name = "Pyroblast", damage = 40, manaCost= 30, castRange = 1)
+
+    def test_Arsenal_equip_weapon_method(self):
+        self.dummy.equip_weapon(weapon=self.weapon)
+    
+    def test_Arsenal_learn_spell_method(self):
+        self.dummy.learn_spell(spell=self.spell)
+
+    def test_Arsenal_getters(self):
         
-        weapon = Weapon(name = "Gorehowl", damage = 20)
-        spell = Spell(name = "Pyroblast", damage = 40, manaCost= 30, castRange = 1)
-        dummy = Arsenal(weapon, spell)
-
-
+        self.dummy.equip_weapon(weapon=self.weapon)
+        
+        with self.subTest("Test Arsenal get weapon method."):
+            self.assertEqual(self.dummy.get_weapon(), self.weapon)
+        
+        self.dummy.learn_spell(spell=self.spell)
+        
+        with self.subTest("Test Arsenal get spell method."):
+            self.assertEqual(self.dummy.get_spell(), self.spell)
+        
 if __name__ == '__main__':
     unittest.main()
