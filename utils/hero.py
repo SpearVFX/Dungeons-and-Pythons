4d1a93e61dc1a4dec2f38240a6a6f92161587136
Shady Dealer <dimiturlukanov@gmail.com>
2018-04-26 18:07:23 +0300

    Finished the hero class.

--- utils/hero.py
+++ utils/hero.py
@@ -1,12 +1,12 @@
 from entity import Entity
-from weapon import Weapon
-from spell import Spell
-
 
 class Hero(Entity):
     def __init__(self, name="", title="", health=100,
                  mana=100, mana_regeneration_rate=2):
 
-        self.super().__init__(name = name, health = health, mana = mana)
+        super().__init__(name = name, health = health, mana = mana)
         self.__title = title
         self.__mana_regeneration_rate = mana_regeneration_rate
+
+    def known_as(self):
+        return f'{super().get_name()} the {self.__title}'
