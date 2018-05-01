6e954880b52ea1c5f62abc00d621001cd5402981
Shady Dealer <dimiturlukanov@gmail.com>
2018-05-02 00:03:45 +0300

    Added a main to test picking up a treasure.

---
+++ utils/dungeons_and_pythons.py
@@ -0,0 +1,18 @@
+import dungeon
+import hero
+
+treasure_map = 'dungeon_maps/test_maps/treasure_map/'
+
+def main():
+
+    dummyDung = dungeon.Dungeon(fileDir= treasure_map)
+    dummyHero = hero.Hero() 
+    dummyDung.spawn(hero=dummyHero)
+    dummyDung.print_map()
+    dummyDung.move_hero(direction = 'down')
+    dummyDung.print_map()
+    print(dummyDung.get_hero().get_weapon())
+    print(dummyDung.get_hero().get_spell())
+
+if __name__== '__main__':
+    main()
