dc0f2969c5c6a35d18ed017a5fc291f395571381
SpearVFX <alexander.kostov37@gmail.com>
2018-05-02 19:39:05 +0300

    Altered attack function, hero and enemy use different functions

--- utils/fight.py
+++ utils/fight.py
@@ -9,12 +9,16 @@ class Fight:
         self.range = self.calculate_range(hero.get_coords(), enemy.get_coords())
 
     def calculate_range(self, heroCoords, enemyCoords):
-        if heroCoords[0] != enemyCoords[0]:
+        if heroCoords[0] == enemyCoords[0]:
+            return abs(heroCoords[1] - enemyCoords[1])
+        elif heroCoords[1] == enemyCoords[1]:
             return abs(heroCoords[0] - enemyCoords[0])
         else:
-            return abs(heroCoords[1] - enemyCoords[1])
+            return -1
 
     def initialize_fight(self):
+        if self.range == -1:
+            print("Invalid fight!!!")
         fight_turn = True
         """
         Since it's a turn based fight the bool will represent the turn:
@@ -44,12 +48,27 @@ class Fight:
         print("Spell or Weapon?   -   Enter s or w")
         command = input()
         if command == "s":
-            if self.hero.can_cast():
+            if self.hero.can_cast() and spell_is_in_range(self.hero):
                 self.enemy.take_damage(spell_.get_damage())
                 self.hero.take_mana(spell_.get_manaCost())
                 print(f'{self.hero.get_name()} dealt {spell_.get_damage()} to {self.enemy.get_name()}')
             else:
-                print("Cannot cast spell.")
+                print("Cannot cast spell. Moving forward 1 UNIT.")
+                self.range -= 1
+        elif command == 'w':
+            if range != 1:
+                print("Cannot attack with weapon. Moving forward 1 UNIT.")
+            else:
+                self.enemy.take_damage(weapon_.get_damage())
+                print(f'{self.hero.get_name()} dealt {weapon_.get_damage()} with {weapon_.get_name()} to {self.enemy.get_name()}')
+    def enemy_attack(self):
+        pass
+    @staticmethod
+    def spell_is_in_range(entity):
+        if entity.get_spell().get_castRange() >= range:
+            return True
+        else:
+            return False
 
 
 
