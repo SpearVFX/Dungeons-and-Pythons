8a708443f831431802e5f0af915b78124413d638
SpearVFX <alexander.kostov37@gmail.com>
2018-05-04 17:10:05 +0300

    Added number stats to the status bar.

--- src/fight_status_bar.py
+++ src/fight_status_bar.py
@@ -1,9 +1,9 @@
-from hero import Hero
-from enemy import Enemy
 import os
 
+
 def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 class FightStatusBar:
     def __init__(self, hero, enemy):
@@ -23,7 +23,7 @@ class FightStatusBar:
         self.health_mana_status_bar()
         pass
 
-    def print_bar(self, stats):
+    def bar_string(self, stats):
         bar_string = ['', '']
         size = int(stats[0] * 50)
         for x in range(0, size):
@@ -35,17 +35,31 @@ class FightStatusBar:
 
         return(bar_string[0], bar_string[1])
 
-    def calculate_ratio(self, e):
+    def get_max_cur_mana_hp(self, e):
         maxHp = e.get_max_health()
         maxMa = e.get_max_mana()
 
         curHp = e.get_health()
         curMa = e.get_mana()
 
-        return (curHp/maxHp, curMa/maxMa)
+        return([(curHp, maxHp), (curMa, maxMa)])
+
+    def calculate_ratio(self, e):
+        stats = self.get_max_cur_mana_hp(e)
+
+        return (stats[0][0]/stats[0][1], stats[1][0]/stats[1][1])  # cur / max
 
     def health_mana_status_bar(self):
-        heroStats = self.print_bar(self.calculate_ratio(self.hero))
-        enemyStats = self.print_bar(self.calculate_ratio(self.enemy))
-        print('|\x1b[31m{:<50}\x1b[0m|\x1b[31m{:<50}\x1b[0m|'.format(heroStats[0], enemyStats[0]))
-        print('|\x1b[34m{:<50}\x1b[0m|\x1b[34m{:<50}\x1b[0m|'.format(heroStats[1], enemyStats[1]))
+        hero_bar = self.bar_string(self.calculate_ratio(self.hero))
+        enemy_bar = self.bar_string(self.calculate_ratio(self.enemy))
+        hero_stats = self.get_max_cur_mana_hp(self.hero)
+        h_health_string = f'{hero_stats[0][0]}/{hero_stats[0][1]}'
+        h_mana_string = f'{hero_stats[1][0]}/{hero_stats[1][1]}'
+        enemy_stats = self.get_max_cur_mana_hp(self.enemy)
+        e_health_string = f'{enemy_stats[0][0]}/{enemy_stats[0][1]}'
+        e_mana_string = f'{enemy_stats[1][0]}/{enemy_stats[1][1]}'
+
+        print('|\x1b[31m{:<50}\x1b[0m|\x1b[31m{:<50}\x1b[0m|'.format(hero_bar[0], enemy_bar[0]))
+        print('|\x1b[31m{:^50}\x1b[0m|\x1b[31m{:^50}\x1b[0m|'.format(h_health_string, e_health_string))
+        print('|\x1b[34m{:<50}\x1b[0m|\x1b[34m{:<50}\x1b[0m|'.format(hero_bar[1], enemy_bar[1]))
+        print('|\x1b[34m{:^50}\x1b[0m|\x1b[34m{:^50}\x1b[0m|'.format(h_mana_string, e_mana_string))
