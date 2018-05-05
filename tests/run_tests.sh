c681de6c72c3e3e2ec53b55ee2356e742504fc24
Shady Dealer <dimiturlukanov@gmail.com>
2018-05-05 13:46:54 +0300

    Modified the bash script to print the name of the current test thats running

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find tests/ -type f -name '*_tests.py' -exec python3.6 -m unittest {} +
+find tests/ -type f -name '*_tests.py' -printf "\n" -print0 -printf "\n" -exec python3.6 -m unittest {} \;
